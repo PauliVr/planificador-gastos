751f5fe6f55b90424069df162c16c204184022f7
PauliVr <paulette.vr.10@gmail.com>
2022-08-06 23:21:42 -0500

    Destructure the props in new budget component and set the onChange method and the value in the input

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
       <form className='formulario '>
         <div className='campo'>
           <label htmlFor=''>Definir Presupuesto</label>
-          <input className='nuevo-presupuesto' type='text' placeholder='Añade tu Presupuesto' />
+          <input
+            className='nuevo-presupuesto'
+            type='text'
+            placeholder='Añade tu Presupuesto'
+            value={presupuesto}
+            onChange={(e) => setPresupuesto(e.target.value)}
+          />
         </div>
         <input type='submit' value='Añadir' />
       </form>
