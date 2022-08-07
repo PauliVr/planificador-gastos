70022142fd935b149ee833505ba48161035eef7e
PauliVr <paulette.vr.10@gmail.com>
2022-08-06 23:18:43 -0500

    Creates the state into the app file

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 import Header from './components/Header';
 
 function App() {
+  const [presupuesto, setPresupuesto] = useState(0);
   return (
     <div>
-      <Header></Header>
+      <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto} />
     </div>
   );
 }
