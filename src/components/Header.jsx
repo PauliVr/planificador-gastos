c0adb73ef029f85ef628745b5beb84b4bc269c85
PauliVr <paulette.vr.10@gmail.com>
2022-08-06 23:20:48 -0500

    Destructure the props and send the props to new budget as props

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import NuevoPresupuesto from './NuevoPresupuesto';
 
-const Header = () => {
+const Header = ({presupuesto, setPresupuesto}) => {
   return (
     <header>
       <h1>Planificador de Gastos</h1>
-      <NuevoPresupuesto></NuevoPresupuesto>
+      <NuevoPresupuesto presupuesto={presupuesto} setPresupuesto={setPresupuesto} />
     </header>
   );
 };
