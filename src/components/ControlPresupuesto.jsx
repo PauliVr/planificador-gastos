01120911ad3eb575f130d665186c4b0c59814cdf
PauliVr <paulette.vr.10@gmail.com>
2022-08-08 17:26:47 -0500

    Creates the New Budget component this component its visible when we insert a valid amount

---
+++ src/components/ControlPresupuesto.jsx
@@ -0,0 +1,31 @@
+const ControlPresupuesto = ({ presupuesto }) => {
+  const formatearCantidad = (cantidad) => {
+    return cantidad.toLocaleString('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    });
+  };
+
+  return (
+    <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
+      <div>
+        <p>Gráfica aquí</p>
+      </div>
+      <div className='contenido-presupuesto'>
+        <p>
+          <span>Presupuesto:</span> {formatearCantidad(presupuesto)}
+        </p>
+
+        <p>
+          <span>Disponible:</span> {formatearCantidad(0)}
+        </p>
+
+        <p>
+          <span>Gastado:</span> {formatearCantidad(0)}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ControlPresupuesto;
