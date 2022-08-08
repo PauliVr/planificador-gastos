2050553350cb690cad4232a9882edb20343cddf3
PauliVr <paulette.vr.10@gmail.com>
2022-08-08 17:23:44 -0500

    Validates the amount in the input

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,20 +1,34 @@
-import React from 'react';
+import { useState } from 'react';
+import Mensaje from './Mensaje';
 
-const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto, setIsValidPresupuesto }) => {
+  const [mensaje, setMensaje] = useState('');
+
+  //validando presupuesto
+  const handlePresupuesto = (e) => {
+    e.preventDefault();
+    if (!presupuesto || presupuesto < 0) {
+      setMensaje('No es un presupuesto valido');
+      return;
+    }
+    setMensaje('');
+    setIsValidPresupuesto(true);
+  };
   return (
     <div className='contenedor-presupuesto contenedor sombra'>
-      <form className='formulario '>
+      <form className='formulario ' onSubmit={handlePresupuesto}>
         <div className='campo'>
           <label htmlFor=''>Definir Presupuesto</label>
           <input
             className='nuevo-presupuesto'
-            type='text'
+            type='number'
             placeholder='Añade tu Presupuesto'
             value={presupuesto}
-            onChange={(e) => setPresupuesto(e.target.value)}
+            onChange={(e) => setPresupuesto(Number(e.target.value))}
           />
         </div>
         <input type='submit' value='Añadir' />
+        {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
       </form>
     </div>
   );
