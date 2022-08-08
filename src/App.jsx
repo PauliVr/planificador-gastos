a87822835350eb47aa4291c0e9df077ffc9442c4
PauliVr <paulette.vr.10@gmail.com>
2022-08-08 17:28:19 -0500

    Adds the new budget button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from 'react';
 import Header from './components/Header';
+import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
   const [presupuesto, setPresupuesto] = useState(0);
+  const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
   return (
     <div>
-      <Header presupuesto={presupuesto} setPresupuesto={setPresupuesto} />
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+        isValidPresupuesto={isValidPresupuesto}
+        setIsValidPresupuesto={setIsValidPresupuesto}
+      />
+
+      <div className='nuevo-gasto'>
+        <img src={IconoNuevoGasto} alt='Icono agregar gasto' />
+      </div>
     </div>
   );
 }
