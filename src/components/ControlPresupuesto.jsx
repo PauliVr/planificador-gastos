d417ab7c884b936dbfb1452823c70c4b4d7f8bc8
PauliVr <paulette.vr.10@gmail.com>
2022-08-09 17:59:22 -0500

    Adds the useEffect and State in the component for show the quantyties

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,16 @@
-const ControlPresupuesto = ({ presupuesto }) => {
+import { useEffect, useState } from 'react';
+
+const ControlPresupuesto = ({ presupuesto, gastos }) => {
+  const [disponible, setDisponible] = useState(0);
+  const [gastado, setGastado] = useState(0);
+
+  useEffect(() => {
+    const totalGastado = gastos.reduce((total, gasto) => gasto.cantidad + total, 0);
+    const totalDisponible = presupuesto - totalGastado;
+    setDisponible(totalDisponible);
+    setGastado(totalGastado);
+  }, [gastos]);
+
   const formatearCantidad = (cantidad) => {
     return cantidad.toLocaleString('en-US', {
       style: 'currency',
@@ -17,11 +29,11 @@ const ControlPresupuesto = ({ presupuesto }) => {
         </p>
 
         <p>
-          <span>Disponible:</span> {formatearCantidad(0)}
+          <span>Disponible:</span> {formatearCantidad(disponible)}
         </p>
 
         <p>
-          <span>Gastado:</span> {formatearCantidad(0)}
+          <span>Gastado:</span> {formatearCantidad(gastado)}
         </p>
       </div>
     </div>
