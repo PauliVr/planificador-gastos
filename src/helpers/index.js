b5554decab40c033ad5b7e7650aef4c1aa25def7
PauliVr <paulette.vr.10@gmail.com>
2022-08-09 14:12:24 -0500

    Creates the format date helper

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,13 @@ export const generarId = () => {
   const fecha = Date.now().toString(36);
   return random + fecha;
 };
+
+export const formatearFecha = (fecha) => {
+  const fechaNueva = new Date(fecha);
+  const opciones = {
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+  };
+  return fechaNueva.toLocaleDateString('es-ES', opciones);
+};
