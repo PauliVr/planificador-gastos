b6197e749c68e7ce86ff7478505eee229ce62707
PauliVr <paulette.vr.10@gmail.com>
2022-08-09 13:15:07 -0500

    Creates the list budget component

---
+++ src/components/ListadoGastos.jsx
@@ -0,0 +1,14 @@
+import Gasto from './Gasto';
+
+const ListadoGastos = ({ gastos }) => {
+  return (
+    <div className='listado-gastos contenedor'>
+      <h2>{gastos.length ? 'Gastos' : 'No hay gastos aun'}</h2>
+      {gastos.map((gasto) => (
+        <Gasto key={gasto.id} gasto={gasto} />
+      ))}
+    </div>
+  );
+};
+
+export default ListadoGastos;
