a544b5e715d56d0e7a0753f43e71e4b6a36ed678
PauliVr <paulette.vr.10@gmail.com>
2022-08-13 14:00:45 -0500

    Imports the component Circular Progress Bar and add the component to the especific container

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const ControlPresupuesto = ({ presupuesto, gastos }) => {
   const [disponible, setDisponible] = useState(0);
@@ -21,7 +23,7 @@ const ControlPresupuesto = ({ presupuesto, gastos }) => {
   return (
     <div className='contenedor-presupuesto contenedor sombra dos-columnas'>
       <div>
-        <p>Gráfica aquí</p>
+        <CircularProgressbar value={0} />
       </div>
       <div className='contenido-presupuesto'>
         <p>
