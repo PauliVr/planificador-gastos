134ddbd3f31cd9e3ae4c418a0adb7134e69c12c7
PauliVr <paulette.vr.10@gmail.com>
2022-08-13 13:38:49 -0500

    Pass the delete expense function as a prop

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,11 +1,16 @@
 import Gasto from './Gasto';
 
-const ListadoGastos = ({ gastos, setGastoEditar }) => {
+const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto }) => {
   return (
     <div className='listado-gastos contenedor'>
       <h2>{gastos.length ? 'Gastos' : 'No hay gastos aun'}</h2>
       {gastos.map((gasto) => (
-        <Gasto key={gasto.id} gasto={gasto} setGastoEditar={setGastoEditar} />
+        <Gasto
+          key={gasto.id}
+          gasto={gasto}
+          setGastoEditar={setGastoEditar}
+          eliminarGasto={eliminarGasto}
+        />
       ))}
     </div>
   );
