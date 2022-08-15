f2045b56ddd63ca08adfb3446b3b6383b7993c74
PauliVr <paulette.vr.10@gmail.com>
2022-08-14 19:18:18 -0500

    Conditional render for the expenses and the expenses filter

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -1,17 +1,33 @@
 import Gasto from './Gasto';
 
-const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto }) => {
+const ListadoGastos = ({ gastos, setGastoEditar, eliminarGasto, filtro, gastosFiltrados }) => {
   return (
     <div className='listado-gastos contenedor'>
-      <h2>{gastos.length ? 'Gastos' : 'No hay gastos aun'}</h2>
-      {gastos.map((gasto) => (
-        <Gasto
-          key={gasto.id}
-          gasto={gasto}
-          setGastoEditar={setGastoEditar}
-          eliminarGasto={eliminarGasto}
-        />
-      ))}
+      {filtro ? (
+        <>
+          <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay gastos en está categoría'}</h2>
+          {gastosFiltrados.map((gasto) => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+          {gastos.map((gasto) => (
+            <Gasto
+              key={gasto.id}
+              gasto={gasto}
+              setGastoEditar={setGastoEditar}
+              eliminarGasto={eliminarGasto}
+            />
+          ))}
+        </>
+      )}
     </div>
   );
 };
